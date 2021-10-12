a967f52dfae5bd4ee4dfefd195ee9a5b31eccbd5
agalup <lupinska.agnieszka@gmail.com>
2021-10-11 18:15:32 -0700

    change instant size

--- main.cu
+++ main.cu
@@ -1,10 +1,9 @@
 #include "pmm.cu"
 using namespace std;
 
-
 int main(int argc, char *argv[]){
 
-    size_t instant_size = 7168ULL * 1024ULL * 1024ULL;
+    size_t instant_size = 1024ULL * 1024ULL * 1024ULL;
 
     int turn_on = 1;
     if (argc > 1){
